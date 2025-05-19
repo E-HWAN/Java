c3a780a0525b026d66100806a6932d0ae75643c0
E-HWAN <Ehwan0466@gmail.com>
2025-05-19 23:22:23 +0900

    First

--- src/chap_04/_03_ElseIf.java
+++ src/chap_04/_03_ElseIf.java
@@ -20,5 +20,40 @@ public class _03_ElseIf {
         else {
             System.out.println("아이스 아메리카노 +1");
         }
+
+
+
+
+        hallabongAde = false;
+        mangojuice = false;
+        boolean orangeJuice = true;
+
+        if (hallabongAde) {
+            System.out.println("한라봉 에이드 +1");
+        }
+        else if (mangojuice) {
+            System.out.println("망고쥬스 +1");
+        }
+        else if (orangeJuice){
+            System.out.println("오랜지쥬스 +1");
+        }
+        else {
+            System.out.println("아이스 아메리카노 +1");
+        }
+
+        hallabongAde = false;
+        mangojuice = false;
+        orangeJuice = false;
+
+        if (hallabongAde) {
+            System.out.println("한라봉 에이드 +1");
+        }
+        else if (mangojuice) {
+            System.out.println("망고쥬스 +1");
+        }
+        else if (orangeJuice){
+            System.out.println("오랜지쥬스 +1");
+        }
+
     }
 }
---
+++ src/chap_04/_04_SwitchCase.java
@@ -0,0 +1,9 @@
+package chap_04;
+
+public class _04_SwitchCase {
+    public static void main(String[] args) {
+
+        // Switch Case
+
+    }
+}
