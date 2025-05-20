5bd97ca72aa12f0ee3807edda419b1826d854942
E-HWAN <Ehwan0466@gmail.com>
2025-05-21 01:11:36 +0900

    first

--- src/chap_04/_05_For.java
+++ src/chap_04/_05_For.java
@@ -50,6 +50,15 @@ public class _05_For {
 
         }
 
+        // 10부터 10까지의 수들의 합
+        // 1 + 2 + 3 + ...
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum = sum + i;
+
+        }
+        System.out.println(sum);
+
 
     }
 }
