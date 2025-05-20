28e801c775cb8db0d50031817813f873c23ea4df
E-HWAN <Ehwan0466@gmail.com>
2025-05-21 01:19:50 +0900

    first

--- src/chap_04/_05_For.java
+++ src/chap_04/_05_For.java
@@ -55,9 +55,10 @@ public class _05_For {
         int sum = 0;
         for (int i = 1; i <= 10; i++) {
             sum = sum + i;
+            System.out.println("현재까지 총 합은 " + sum + "입니다.");
 
         }
-        System.out.println(sum);
+        System.out.println("1부터 10까지의 모든 수의 총합은 " + sum + "입니다.");
 
 
     }
