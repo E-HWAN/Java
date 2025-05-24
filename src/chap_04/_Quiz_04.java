073a002102b97ffd4e53797f01158cf0819abb47
E-HWAN <Ehwan0466@gmail.com>
2025-05-24 22:22:43 +0900

    first

--- src/chap_04/_Quiz_04.java
+++ src/chap_04/_Quiz_04.java
@@ -1,34 +1,28 @@
 package chap_04;
-import com.sun.org.apache.xerces.internal.xs.XSModel;
-
 import java.util.Scanner;
-
 public class _Quiz_04 {
     public static void main(String[] args) {
-
-        Scanner inputSmallCar = new Scanner(System.in);
-        System.out.println("경차 인가요?");
-        boolean smallCar = inputSmallCar.nextBoolean();
+        Scanner inputSmallcar = new Scanner(System.in);
+        System.out.println("경차/장애인 입니까?");
+        boolean smallcar = inputSmallcar.nextBoolean();
 
         Scanner inputTime = new Scanner(System.in);
-        System.out.println("몇시간 주차 하셨나요?");
+        System.out.println("몇시간 주차 하셨습니까?");
         double time = inputTime.nextDouble();
 
         double money = time * 4000;
 
-        if (money>30000){
+        if (money > 30000){
             money = 30000;
         }
 
-        if (smallCar){
+        if (smallcar){
             money = money/2;
         }
 
-        int result = (int)money;
 
-
-
-        System.out.println(result + "원 입니다.");
+        int result = (int) money;
+        System.out.println("요금은 " + result + "원 입니다.");
 
     }
 }
